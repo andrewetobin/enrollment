227995dd115e751900086736b9ec47ce9a44107e
andrewetobin <andrewetobin@gmail.com>
2018-08-28 15:36:44 -0600

    tests relationships for student

--- spec/models/student_spec.rb
+++ spec/models/student_spec.rb
@@ -4,4 +4,10 @@ describe Student, type: :model do
   describe "validations" do
     it {should validate_presence_of(:name)}
   end
+  describe "relationships" do
+    it "has many addresses" do
+      student =  Student.create(name: "Cynthia")
+      expect(student).to respond_to(:addresses)
+    end
+  end
 end
